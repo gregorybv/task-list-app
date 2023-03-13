c1a96a8a934c5a70df8a917a68ca8819c94e7cf5
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-13 03:50:22 +0300

    add GiHornedHelm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react"
+import { GiHornedHelm } from "react-icons/gi"
 
 function App() {
   return (
     <div className='app'>
       <div className='container'>
-        <h1>PowerList</h1>
+        <h1>
+          <GiHornedHelm /> PowerList
+        </h1>
       </div>
     </div>
   )
